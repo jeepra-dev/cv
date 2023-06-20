5b83d53d176cb7119cfb79ae63b78c9995c34407
Mamadou <sallmahdou1@gmail.com>
2023-06-20 03:17:40 +0200

    add some rules for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,30 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
-    'react/react-in-jsx-scope': 0,
+    'unicode-bom': 'error',
+    'vars-on-top': 'warn',
+    'promise/no-nesting': 'off',
+    'prefer-const': 'error',
+    'valid-jsdoc': 'warn',
+    'use-isnan': 'warn',
+    'no-sparse-arrays': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-unreachable': 'warn',
+    'no-console': 'warn',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    /*        "@typescript-eslint/naming-convention": [
+          "warn"
+      ], */
+    '@typescript-eslint/no-empty-function': ['warn'],
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 }
